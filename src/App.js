1d41835856b922e6546fdcbf549290fe1bc1346f
ezra-tarab <ezra5385@gmail.com>
2024-01-23 17:07:20 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./App.module.css";
-import APIService from "./commponents/service/APIService";
 import DeletePatient from "./commponents/DeletePatient";
 import Doctors from "./commponents/pages/Doctors";
 import Home from "./commponents/pages/Home";
@@ -10,46 +9,38 @@ import {
   Navigate,
   Route,
   Routes,
-  Switch,
 } from "react-router-dom";
 import SignUp from "./commponents/pages/SignUp";
 import About from "./commponents/pages/About";
 import NavBar from "./commponents/NavBar";
-import backVid from "./commponents/assets/video.mp4";
-import BubbleHome from "./commponents/BubbleHome";
-import image from "./commponents/assets/back.jpeg";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ContactUs from "./commponents/pages/ContactUs";
-import Personal from "./commponents/pages/Personal";
 import { UserProvider } from "./commponents/Context";
 import ForgotPassword from "./commponents/pages/ForgotPassword";
 import LogOut from "./commponents/pages/LogOut";
 
 function App() {
-
-  
   return (
     <div>
       <BrowserRouter>
-      <UserProvider>
-        <NavBar />
-        <Routes>
-          <Route path="/doctors/doctors" element={<Doctors />} />
-          <Route path="/patients/patients" element={<PatientList />} />
-          <Route
-            path="/patients/deletePatient/:id"
-            element={<DeletePatient />}
-          />
-          <Route path="/home" element={<Home />} />
-          <Route path="/login" element={<LogIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contactus" element={<ContactUs/>} />
-          <Route path="/forgotPassword" element={<ForgotPassword/>} />
-          <Route path="/" element={<ForgotPassword/>} />
-          <Route path="/logout" element={<LogOut/>} />
-          <Route exact path="/" element={<Navigate to="/home" />} />
-        </Routes>
+        <UserProvider>
+          <NavBar />
+          <Routes>
+            <Route path="/doctors/doctors" element={<Doctors />} />
+            <Route path="/patients/patients" element={<PatientList />} />
+            <Route
+              path="/patients/deletePatient/:id"
+              element={<DeletePatient />}
+            />
+            <Route path="/home" element={<Home />} />
+            <Route path="/login" element={<LogIn />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contactus" element={<ContactUs />} />
+            <Route path="/forgotPassword" element={<ForgotPassword />} />
+            <Route path="/logout" element={<LogOut />} />
+            <Route path="/" element={<Navigate replace to="/home"/>} />
+          </Routes>
         </UserProvider>
       </BrowserRouter>
     </div>
--- src/commponents/BubbleHome.js
+++ src/commponents/BubbleHome.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
 import styles from "./BubbleHome.module.css";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "./Context";
 
 export default function BubbleHome() {
+  const { isLogin } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  function handleClickButtons() {
+    isLogin ? navigate("/home") : navigate("/login");
+  }
+
   return (
     <div className={styles.buttons}>
-      <button> Schedule an appointment</button>
-      <button>Change an appointment</button>
-      <button>Test results</button>
-      <button>Doctors</button>
+      <button onClick={handleClickButtons}>Schedule an appointment</button>
+      <button onClick={handleClickButtons}>Change an appointment</button>
+      <button onClick={handleClickButtons}>Test results</button>
+      <button onClick={() => navigate("/doctors/doctors")}>Doctors</button>
     </div>
   );
 }
